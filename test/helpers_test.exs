fd5fb4afa2b78119308517220904b527594d1def
Lizzie Paquette <lizziepaquette@gmail.com>
2019-04-03 14:46:17 -0700

    version 0.1.3
    removed:
    	partition/1
    	match_pred/1
    	on_match/3
    
    other changes:
    	split functions into files by category
    	s/~>>/~>/g

--- test/helpers_test.exs
+++ test/helpers_test.exs
@@ -1,100 +1,10 @@
-defmodule ResultTest do
+defmodule HelpersTest do
   @moduledoc false
   use ExUnit.Case
-  import Result
 
-  doctest Result
+  import Result.Helpers
 
-  # TODO: test side effects
-  def sgn(x) do
-    if x > 0 do
-      {:ok, "pos"}
-    else
-      {:error, "neg"}
-    end
-  end
-
-  def two_args(x, y), do: {:ok, x - y}
-
-  def inside_bind(x, f), do: x ~>> f.()
-
-  test "bind" do
-    assert_raise FunctionClauseError, fn ->
-      bind(:ok, &sgn/1)
-    end
-
-    assert {:ok, "pos"} = bind({:ok, 1}, &sgn/1)
-
-    assert {:error, "neg"} = bind({:ok, -4}, &sgn/1)
-
-    assert {:error, -4} = bind({:error, -4}, &sgn/1)
-
-    assert {:ok, :bar} = bind({:ok, :foo}, fn _ -> {:ok, :bar} end)
-
-    assert {:error, :foo} = bind({:error, :foo}, fn _ -> {:ok, :bar} end)
-
-    assert_raise ArgumentError, fn ->
-      bind({:ok, 2}, fn _ -> :ok end)
-    end
-
-    assert_raise ArgumentError, fn ->
-      bind({:ok, 2}, & &1)
-    end
-  end
-
-  test "~>>" do
-    assert_raise FunctionClauseError, fn ->
-      :ok ~>> sgn
-    end
-
-    assert {:ok, "pos"} = {:ok, 1} ~>> sgn
-
-    assert {:error, "neg"} = {:ok, -3} ~>> sgn
-
-    assert {:error, 2} = {:error, 2} ~>> sgn
-
-    assert {:ok, 1} = {:ok, 3} ~>> two_args(2)
-
-    assert {:ok, 1} = {:ok, 3} ~>> two_args(2)
-
-    assert {:ok, 1} = {:ok, 3} ~>> two_args(2)
-
-    assert {:error, 1} = {:ok, 1} |> inside_bind(fn x -> {:error, x} end)
-
-    assert {:ok, 2} = {:ok, 1} |> inside_bind(fn x -> {:ok, x + 1} end)
-
-    assert {:error, 1} = {:error, 1} |> inside_bind(&{:ok, &1})
-
-    assert_raise ArgumentError, fn ->
-      {:ok, 1} ~>> (fn _x -> raise(ArgumentError) end).()
-    end
-
-    assert {:ok, 2} =
-             {:ok, 1} ~>> (fn x -> if x > 1, do: raise(ArgumentError), else: {:ok, 2} end).()
-  end
-
-  test "fmap" do
-    assert_raise FunctionClauseError, fn ->
-      :ok
-      |> fmap(fn x -> x + 2 end)
-    end
-
-    assert {:ok, 8} =
-             {:ok, 6}
-             |> fmap(fn x -> x + 2 end)
-
-    assert {:error, 6} =
-             {:error, 6}
-             |> fmap(fn x -> x + 2 end)
-  end
-
-  test "ignore" do
-    assert :ok = ignore({:ok, 2})
-
-    assert :ok = ignore(:ok)
-
-    assert {:error, :not_found} = ignore({:error, :not_found})
-  end
+  doctest Result.Helpers
 
   test "normalize_error" do
     assert {:error, :test} = normalize_error(:error, :test)
@@ -106,14 +16,6 @@ defmodule ResultTest do
     assert {:ok, 2} = normalize_error({:ok, 2})
 
     assert :ok = normalize_error(:ok)
-  end
-
-  test "extract!" do
-    assert 1 = extract!({:ok, 1})
-
-    assert_raise ArgumentError, fn -> extract!({:error, 1}) end
-
-    assert_raise FunctionClauseError, fn -> extract!(:ok) end
   end
 
   test "lift" do
@@ -268,52 +170,5 @@ defmodule ResultTest do
     assert {:ok, 3} =
              {:error, 1}
              |> convert_error(&(1 == &1), &{:ok, 2 + &1})
-  end
-
-  test "convert_error with side effect" do
-    # TODO: write side effect tests
-  end
-
-  test "map_with_bind" do
-    assert [{:ok, 6}, {:error, 12}, {:error, 3}, {:ok, 24}] =
-             [{:ok, 1}, {:ok, 2}, {:error, 3}, {:ok, 4}]
-             |> map_with_bind(fn x -> if x == 2, do: {:error, x * 6}, else: {:ok, x * 6} end)
-  end
-
-  test "map_while_success" do
-    assert {:error, 1} =
-             [1, 2, 3, 4]
-             |> map_while_success(fn x -> if x == 3 || x == 1, do: {:error, x}, else: {:ok, x} end)
-
-    assert {:ok, [3, 4, 5, 6]} = map_while_success([1, 2, 3, 4], fn x -> {:ok, x + 2} end)
-  end
-
-  test "reduce_while_success/3" do
-    # Note: the order is wacky
-    assert {:ok, "dcbae"} =
-             ["a", "b", "c", "d"]
-             |> reduce_while_success({:ok, "e"}, &{:ok, &1 <> &2})
-
-    assert {:error, "test"}
-
-    ["a", "b", "c", "d"]
-    |> reduce_while_success({:error, "test"}, &{:ok, &1 <> &2})
-
-    assert {:error, "B!"} =
-             ["a", "b", "c", "d"]
-             |> reduce_while_success(
-               {:ok, "e"},
-               fn x, acc -> if x == "b", do: {:error, "B!"}, else: {:ok, x <> acc} end
-             )
-  end
-
-  test "each_while_success" do
-    assert {:error, :too_big} =
-             [1, 2, 3, 4]
-             |> each_while_success(fn x -> if x < 3, do: :ok, else: {:error, :too_big} end)
-
-    assert :ok =
-             [1, 2, 3, 4]
-             |> each_while_success(fn x -> if x < 5, do: :ok, else: {:error, :too_big} end)
   end
 end
