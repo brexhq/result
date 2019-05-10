29ddb138c4d51195c4f1473039b30956c44ff6c6
Lizzie Paquette <lizziepaquette@gmail.com>
2019-05-10 10:41:59 -0700

    version 0.4.1
    changes:
    	better to_string for reason when logging

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Brex.Result.MixProject do
   def project do
     [
       app: :brex_result,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
